2b4b8514ddd989f7a74702e8dc8f768200987793
Elena Dibirova <shpitalnik@bk.ru>
2022-03-21 23:08:36 +0300

    fix review issues

--- script/index.js
+++ script/index.js
@@ -1,42 +1,38 @@
 const popupElement = document.querySelector('.popup');
 const editButton = document.querySelector('.profile__edit-button');
 const closeButton = popupElement.querySelector('.popup__close-button');
-
 let formElement = popupElement.querySelector('.popup__window')
-let nameInput = formElement.querySelector('.popup__name-edit');
-let jobInput = formElement.querySelector('.popup__bio-edit');
+let nameInput = formElement.querySelector('.popup__name-edit_theme_profile');
+let jobInput = formElement.querySelector('.popup__bio-edit_theme_profile');
+let profileName = document.querySelector('.profile__name');
+let profileBio = document.querySelector('.profile__bio');
+// const ESC_KEY = "Escape";
 
 function openPopup () {
+  nameInput.value = profileName.textContent
+  jobInput.value = profileBio.textContent
   popupElement.classList.add('popup_opened');
-  document.addEventListener('keyup', onDocumentKeyUp);
-
-  nameInput.value = document.querySelector('.profile__name').textContent
-  jobInput.value = document.querySelector('.profile__bio').textContent
+  // document.addEventListener('keyup', onDocumentKeyUp);
 }
 
 function closePopup() {
   popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keyup', onDocumentKeyUp);
+  // document.removeEventListener('keyup', onDocumentKeyUp);
 }
-
-function onDocumentKeyUp (event) {
-  if(event.key === ESC_KEY) {
-    closePopup();
-  }
-}
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-
-const ESC_KEY = "Escape";
 
 function formSubmitHandler (evt) {
   evt.preventDefault();
-
-  document.querySelector('.profile__name').textContent = nameInput.value
-  document.querySelector('.profile__bio').textContent = jobInput.value
-  
+  profileName.textContent = nameInput.value
+  profileBio.textContent = jobInput.value
   closePopup();
 }
 
+// function onDocumentKeyUp (event) {
+//   if(event.key === ESC_KEY) {
+//     closePopup();
+//   }
+// }
+
 formElement.addEventListener('submit', formSubmitHandler);
+editButton.addEventListener('click', openPopup);  
+closeButton.addEventListener('click', closePopup);
